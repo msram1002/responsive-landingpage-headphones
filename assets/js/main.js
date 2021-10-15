2ffcd01a97221949278793f1a544dba070e7e31e
Sundara Ram Matta <msram0210@gmail.com>
2021-10-14 20:20:38 -0400

    Hiding the menu when we click on a link for mobile specific

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,13 @@
 /*=============== SHOW MENU ===============*/
 const navMenu = document.getElementById('nav-menu'),
   navToggle = document.getElementById('nav-toggle'),
-  navClose = document.getElementById('nav-close')
+  navClose = document.getElementById('nav-close');
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
 if (navToggle) {
   navToggle.addEventListener('click', () => {
-    navMenu.classList.add('show-menu')
+    navMenu.classList.add('show-menu');
   })
 }
 
@@ -15,12 +15,20 @@ if (navToggle) {
 /* Validate if constant exists */
 if (navClose) {
   navClose.addEventListener('click', () => {
-    navMenu.classList.remove('show-menu')
+    navMenu.classList.remove('show-menu');
   })
 }
 
-/*=============== REMOVE MENU MOBILE ===============*/
+/*=============== HIDE MENU MOBILE ===============*/
 
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction() {
+  const navMenu = document.getElementById('nav-menu');
+  // When we click on each nav link, we remove the show-menu class
+  navMenu.classList.remove('show-menu');
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 
